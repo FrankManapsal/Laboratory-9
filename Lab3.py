367c609af9bb7483ab3b74392e4491e4b5340173
Micah Manapsal <2024-2-1151@lpunetwork.edu.ph>
2024-09-23 15:10:37 +0800

    Second Commit

--- Lab3.py
+++ Lab3.py
@@ -1,1 +1,13 @@
-print("hello mga potaginano")
+salary=int(input("What is your Monthly Salary?: "))
+if salary>=30000:
+    loan=int(input("What is your Desired Loan: "))
+    if loan<=salary*10:
+        print("You are Eligible for a Loan!")
+        monthsforpayment=int(input("How many Months you want to Pay the Loan?:"))
+        interest=loan*0.10
+        totalpayment=float(loan+interest)
+        print("Amount to Pay:",totalpayment)
+    else:
+        print("Uneligible for Loan.(Loan too High.)")
+else:
+    print("Uneligible for Loan(Salary too Low.)")
